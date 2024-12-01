ed543a036e526a9667ec9972556b48d02239909d
EsDeath <amadeussystem04@gmail.com>
2024-12-01 19:38:54 +0530

    changed Metadata and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,43 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
 import "./globals.css";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+export const metadata: Metadata = {
+  title: "EmotionAI",
+  description: "A JavaScipt Based Emotion detection System",
+  icons: {
+    icon: "/public/assets/favicon.png",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
